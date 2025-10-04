17fa0806a017b103f5f45a1d7ec35e968836a096
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-10-04 15:44:18 -0700

    root folder restructure

--- gamesource/main.gd
+++ gamesource/main.gd
@@ -1,9 +1,10 @@
 extends Node
 
 func _ready():
+	print("test")
 	$HTTPRequest.request_completed.connect(_on_request_completed)
-	$HTTPRequest.request("https://api.github.com/repos/godotengine/godot/releases/latest")
+	$HTTPRequest.request("https://dog.ceo/api/breeds/image/random")
 
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json["name"])
+	print(json["message"])
