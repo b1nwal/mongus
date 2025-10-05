dfd7ac3f69256f9e8f4a721fc53ac4566e496554
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-10-05 05:33:14 -0700

    First commit in a long time; made inventory work with ormm

--- gamesource/main.gd
+++ gamesource/main.gd
@@ -6,12 +6,26 @@ var TweakerScene = preload("res://enemy/tweaker.tscn")
 
 @onready var experience_bar = $SBPlayer/ExperienceBar
 @onready var health_bar = $SBPlayer/HealthBar
+@onready var ormm_request = $OrmmRequest
+
 
 func _ready():
+	ormm_request.request("octopus blade")
+	ormm_request.request("water blade")
+	ormm_request.request("jogging blade")
+	ormm_request.request("music blade")
+	ormm_request.request("heat blade")
+	ormm_request.request("dog blade")
+	ormm_request.request("fog blade")
+	ormm_request.request("green blade")
+	ormm_request.request("red blade")
+	ormm_request.request("blood blade")
+	ormm_request.request("glue blade")
+	ormm_request.request("gun blade")
 	spawn_enemy()
 	
 func spawn_enemy():
-	for i in 1:
+	for i in 10:
 		var tweaker = TweakerScene.instantiate()
 		tweaker.position = Vector2(randi_range(-600,600),randi_range(-600,600))
 		tweaker.target = $SBPlayer
---
+++ gamesource/scripts/inventory.gd
@@ -0,0 +1,22 @@
+# inventory.gd
+extends Node
+class_name Inventory
+
+var items: Array = []
+
+var image_count := 0
+const IMAGE_SIZE := Vector2(256, 256)
+
+
+
+func add_item(item: Weapon):
+	items.append(item)
+	print("Added ", item.weapon_info.name)
+	if len(items) > 9:
+		items.pop_back()
+		
+
+#func remove_item(item: Weapon):
+	#if item in items:
+		#items.erase(item)
+		#print("Removed", item.weapon_info.name)
