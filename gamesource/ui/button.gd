e17943bca7bcedbad0d88d364377c943122c2cf0
panyongpin <handragon11@gmail.com>
2025-10-05 02:57:13 -0700

    adding a home menu

---
+++ gamesource/ui/button.gd
@@ -0,0 +1,13 @@
+extends Button
+
+func ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _on_button_pressed():
+	print("roweijroweirj")
+	get_tree().change_scene_to_file("res://main.tscn")
+	
+	pass
