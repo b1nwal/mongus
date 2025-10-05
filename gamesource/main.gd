585d08d3584ac59bb4d887fbe7c4192e159a6eb8
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-10-04 20:15:32 -0700

    rebasing to player-based movement

--- gamesource/main.gd
+++ gamesource/main.gd
@@ -18,19 +18,3 @@ func _on_ai_response(success: bool, data):
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	print(json["message"])
-
-var player_speed = 400;
-
-@onready var worldnode = $WorldNode
-
-func _physics_process(delta):
-	var movement = Vector2.ZERO
-	if Input.is_action_pressed("ui_right"):
-		movement.x -= player_speed * delta
-	if Input.is_action_pressed("ui_left"):
-		movement.x += player_speed * delta
-	if Input.is_action_pressed("ui_down"):
-		movement.y -= player_speed * delta
-	if Input.is_action_pressed("ui_up"):
-		movement.y += player_speed * delta
-	worldnode.position += movement
