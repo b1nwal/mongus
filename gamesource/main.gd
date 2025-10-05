db4cadbf97ef07cd9a4e460313bfe70a8d2fd9d0
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-10-04 17:03:24 -0700

    added movement

--- gamesource/main.gd
+++ gamesource/main.gd
@@ -8,3 +8,19 @@ func _ready():
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	print(json["message"])
+
+var player_speed = 400;
+
+@onready var worldnode = $WorldNode
+
+func _physics_process(delta):
+	var movement = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		movement.x -= player_speed * delta
+	if Input.is_action_pressed("ui_left"):
+		movement.x += player_speed * delta
+	if Input.is_action_pressed("ui_down"):
+		movement.y -= player_speed * delta
+	if Input.is_action_pressed("ui_up"):
+		movement.y += player_speed * delta
+	worldnode.position += movement
