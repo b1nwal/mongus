d9d7676ed7a23a233b67026832d6e8317756c6a0
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-10-04 21:26:28 -0700

    Fixed inheritance structure

--- gamesource/enemy/testenemy.gd
+++ gamesource/enemy/testenemy.gd
@@ -3,6 +3,9 @@ extends "res://scripts/entity.gd"
 var damage = 20
 var target
 
+func _ready():
+	speed = 20
+
 func _process(delta):
 	print(global_position.distance_to(Vector2(0,0)))
 	var direction = (target.position - position).normalized()
@@ -10,5 +13,6 @@ func _process(delta):
 	if collision:
 		if collision.get_collider() == target:
 			print("hit!")
+
 func attack_player():
 	target.incur(damage)
