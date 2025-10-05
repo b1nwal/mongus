95c6472e4d9b4b353a73759793d3de56eb66b59b
panyongpin <handragon11@gmail.com>
2025-10-04 20:28:12 -0700

    movement fix with 8 if statements

--- gamesource/main.gd
+++ gamesource/main.gd
@@ -25,20 +25,29 @@ var player_speed = 400;
 
 func _physics_process(delta):
 	var movement = Vector2.ZERO
-	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_up"):
+	var pleft = Input.is_action_pressed("ui_left")
+	var pright = Input.is_action_pressed("ui_right")
+	var pup = Input.is_action_pressed("ui_up")
+	var pdown = Input.is_action_pressed("ui_down")
+	
+	if pright and pup:
 		movement.x -= player_speed / 1.4 * delta
 		movement.y += player_speed / 1.4 * delta
-	if Input.is_action_pressed("ui_right"):
-		movement.x -= player_speed * delta
-	if Input.is_action_pressed("ui_left"):
+	elif pright and pdown:
+		movement.x -= player_speed / 1.4 * delta
+		movement.y -= player_speed / 1.4 * delta
+	elif pleft and pup:
+		movement.x += player_speed / 1.4 * delta
+		movement.y += player_speed / 1.4 * delta
+	elif pleft and pdown:
+		movement.x += player_speed / 1.4 * delta
+		movement.y -= player_speed / 1.4 * delta
+	elif pleft:
 		movement.x += player_speed * delta
-	if Input.is_action_pressed("ui_down"):
-		movement.y -= player_speed * delta
-	if Input.is_action_pressed("ui_up"):
+	elif pright:
+		movement.x -= player_speed * delta
+	elif pup:
 		movement.y += player_speed * delta
-	#if movement:
-		#movement.normalize()
-	#else:
-		#Vector2.ZERO
-	#
+	elif pdown:
+		movement.y -= player_speed * delta
 	worldnode.position += movement
