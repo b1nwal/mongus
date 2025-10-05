38e5a456bb2911d5367b2908a632cf47df76cab6
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-10-04 17:15:30 -0700

    I don't know what these changes are. I'm committing for the lols

--- gamesource/main.gd
+++ gamesource/main.gd
@@ -14,7 +14,7 @@ func _on_ai_response(success: bool, data):
 		print("AI Response:", data)
 	else:
 		print("Error:", data)
-    
+	
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	print(json["message"])
